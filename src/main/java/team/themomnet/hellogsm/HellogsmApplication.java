4ce02b5430a720dd1445c5a46976d220b6e3247d
YangSiJun528 <yangsijun5528@gmail.com>
2023-12-15 09:25:36 +0900

    first commit

---
+++ src/main/java/team/themomnet/hellogsm/HellogsmApplication.java
@@ -0,0 +1,13 @@
+package team.themomnet.hellogsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellogsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellogsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/themomnet/hellogsm/HellogsmApplicationTests.java
@@ -0,0 +1,13 @@
+package team.themomnet.hellogsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellogsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
